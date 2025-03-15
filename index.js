e605687ff360af57b920eaf72a7fc059c71f197a
Tiwarishraddha <shraddhatiwari2406@gmail.com>
2025-03-15 18:41:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+//port findout
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+
+const blogs = require("./routes/blog")
+
+//mount
+app.use("/api/v1", blog);
+
+//database fetch
+const connectWithDb = require("./config/database");
+connectWithDb();
+
+//start the server
+app.listen(PORT,() => {
+    console.log(`App is running at ${PORT}`);
+});
+
+//default route
+app.get("/", (req,res) => {
+    res.send(`<h1>This is homepage2</h1>`)
+})
